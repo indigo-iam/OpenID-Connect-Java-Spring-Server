bce4b0ac3c55003de6bc97d6a9a04ef235972654
Federica Agostini <federica.agostini@cnaf.infn.it>
2024-06-05 14:43:20 +0200

    Add public method to interface

--- openid-connect-common/src/main/java/org/mitre/oauth2/service/OAuth2TokenEntityService.java
+++ openid-connect-common/src/main/java/org/mitre/oauth2/service/OAuth2TokenEntityService.java
@@ -20,6 +20,7 @@ package org.mitre.oauth2.service;
 import java.util.List;
 import java.util.Set;
 
+import org.mitre.oauth2.model.AuthenticationHolderEntity;
 import org.mitre.oauth2.model.ClientDetailsEntity;
 import org.mitre.oauth2.model.OAuth2AccessTokenEntity;
 import org.mitre.oauth2.model.OAuth2RefreshTokenEntity;
@@ -28,39 +29,40 @@ import org.springframework.security.oauth2.provider.token.AuthorizationServerTok
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 @SuppressWarnings("deprecation")
-public interface OAuth2TokenEntityService
-    extends AuthorizationServerTokenServices, ResourceServerTokenServices {
+public interface OAuth2TokenEntityService extends AuthorizationServerTokenServices, ResourceServerTokenServices {
 
-  @Override
-  public OAuth2AccessTokenEntity readAccessToken(String accessTokenValue);
+	@Override
+	public OAuth2AccessTokenEntity readAccessToken(String accessTokenValue);
 
-  public OAuth2RefreshTokenEntity getRefreshToken(String refreshTokenValue);
+	public OAuth2RefreshTokenEntity getRefreshToken(String refreshTokenValue);
 
-  public void revokeRefreshToken(OAuth2RefreshTokenEntity refreshToken);
+	public void revokeRefreshToken(OAuth2RefreshTokenEntity refreshToken);
 
-  public void revokeAccessToken(OAuth2AccessTokenEntity accessToken);
+	public void revokeAccessToken(OAuth2AccessTokenEntity accessToken);
 
-  public List<OAuth2AccessTokenEntity> getAccessTokensForClient(ClientDetailsEntity client);
+	public List<OAuth2AccessTokenEntity> getAccessTokensForClient(ClientDetailsEntity client);
 
-  public List<OAuth2RefreshTokenEntity> getRefreshTokensForClient(ClientDetailsEntity client);
+	public List<OAuth2RefreshTokenEntity> getRefreshTokensForClient(ClientDetailsEntity client);
 
-  public void clearExpiredTokens();
+	public void clearExpiredTokens();
 
-  public OAuth2AccessTokenEntity saveAccessToken(OAuth2AccessTokenEntity accessToken);
+	public OAuth2AccessTokenEntity saveAccessToken(OAuth2AccessTokenEntity accessToken);
 
-  public OAuth2RefreshTokenEntity saveRefreshToken(OAuth2RefreshTokenEntity refreshToken);
+	public OAuth2RefreshTokenEntity saveRefreshToken(OAuth2RefreshTokenEntity refreshToken);
 
-  @Override
-  public OAuth2AccessTokenEntity getAccessToken(OAuth2Authentication authentication);
+	@Override
+	public OAuth2AccessTokenEntity getAccessToken(OAuth2Authentication authentication);
 
-  public OAuth2AccessTokenEntity getAccessTokenById(Long id);
+	public OAuth2AccessTokenEntity getAccessTokenById(Long id);
 
-  public OAuth2RefreshTokenEntity getRefreshTokenById(Long id);
+	public OAuth2RefreshTokenEntity getRefreshTokenById(Long id);
 
-  public Set<OAuth2AccessTokenEntity> getAllAccessTokensForUser(String name);
+	public Set<OAuth2AccessTokenEntity> getAllAccessTokensForUser(String name);
 
-  public Set<OAuth2RefreshTokenEntity> getAllRefreshTokensForUser(String name);
+	public Set<OAuth2RefreshTokenEntity> getAllRefreshTokensForUser(String name);
 
-  public OAuth2AccessTokenEntity getRegistrationAccessTokenForClient(ClientDetailsEntity client);
+	public OAuth2AccessTokenEntity getRegistrationAccessTokenForClient(ClientDetailsEntity client);
+
+	public OAuth2RefreshTokenEntity createRefreshToken(ClientDetailsEntity client, AuthenticationHolderEntity authHolder);
 
 }

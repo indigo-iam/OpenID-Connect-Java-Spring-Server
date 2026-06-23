2e1f098ad2ad3f9b74234405032b9459c3991ffd
enricovianello <maverick17586@gmail.com>
2026-06-23 19:03:00 +0200

    Remove more classes

--- openid-connect-common/src/main/java/org/mitre/oauth2/model/OAuth2RefreshTokenEntity.java
+++ openid-connect-common/src/main/java/org/mitre/oauth2/model/OAuth2RefreshTokenEntity.java
@@ -20,11 +20,10 @@
  */
 package org.mitre.oauth2.model;
 
+import java.text.ParseException;
 import java.util.Date;
 
-import javax.persistence.Basic;
 import javax.persistence.Column;
-import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -32,163 +31,91 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.Transient;
 
-import org.mitre.oauth2.model.convert.JWTStringConverter;
 import org.springframework.security.oauth2.common.OAuth2RefreshToken;
 
-import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.PlainJWT;
 
-/**
- * @author jricher
- *
- */
+@SuppressWarnings("deprecation")
 @Entity
 @Table(name = "refresh_token")
-@NamedQueries({
-	@NamedQuery(name = OAuth2RefreshTokenEntity.QUERY_ALL, query = "select r from OAuth2RefreshTokenEntity r"),
-	@NamedQuery(name = OAuth2RefreshTokenEntity.QUERY_EXPIRED_BY_DATE, query = "select r from OAuth2RefreshTokenEntity r where r.expiration <= :" + OAuth2RefreshTokenEntity.PARAM_DATE),
-	@NamedQuery(name = OAuth2RefreshTokenEntity.QUERY_BY_CLIENT, query = "select r from OAuth2RefreshTokenEntity r where r.client = :" + OAuth2RefreshTokenEntity.PARAM_CLIENT),
-	@NamedQuery(name = OAuth2RefreshTokenEntity.QUERY_BY_TOKEN_VALUE, query = "select r from OAuth2RefreshTokenEntity r where r.jwt = :" + OAuth2RefreshTokenEntity.PARAM_TOKEN_VALUE),
-	@NamedQuery(name = OAuth2RefreshTokenEntity.QUERY_BY_NAME, query = "select r from OAuth2RefreshTokenEntity r where r.authenticationHolder.userAuth.name = :" + OAuth2RefreshTokenEntity.PARAM_NAME)
-})
 public class OAuth2RefreshTokenEntity implements OAuth2RefreshToken {
 
-	public static final String QUERY_BY_TOKEN_VALUE = "OAuth2RefreshTokenEntity.getByTokenValue";
-	public static final String QUERY_BY_CLIENT = "OAuth2RefreshTokenEntity.getByClient";
-	public static final String QUERY_EXPIRED_BY_DATE = "OAuth2RefreshTokenEntity.getAllExpiredByDate";
-	public static final String QUERY_ALL = "OAuth2RefreshTokenEntity.getAll";
-	public static final String QUERY_BY_NAME = "OAuth2RefreshTokenEntity.getByName";
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
 
-	public static final String PARAM_TOKEN_VALUE = "tokenValue";
-	public static final String PARAM_CLIENT = "client";
-	public static final String PARAM_DATE = "date";
-	public static final String PARAM_NAME = "name";
+  @ManyToOne
+  @JoinColumn(name = "auth_holder_id")
+  private AuthenticationHolderEntity authenticationHolder;
 
-	private Long id;
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "client_id")
+  private ClientDetailsEntity client;
 
-	private AuthenticationHolderEntity authenticationHolder;
+  @Column(name = "token_value")
+  private String value;
 
-	private ClientDetailsEntity client;
+  @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+  @Column(name = "expiration")
+  private Date expiration;
 
-	//JWT-encoded representation of this access token entity
-	private JWT jwt;
+  public OAuth2RefreshTokenEntity() {
 
-	// our refresh tokens might expire
-	private Date expiration;
+  }
 
-	/**
-	 *
-	 */
-	public OAuth2RefreshTokenEntity() {
+  public Long getId() {
+    return id;
+  }
 
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	/**
-	 * @return the id
-	 */
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	public Long getId() {
-		return id;
-	}
+  public AuthenticationHolderEntity getAuthenticationHolder() {
+    return authenticationHolder;
+  }
 
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public void setAuthenticationHolder(AuthenticationHolderEntity authenticationHolder) {
+    this.authenticationHolder = authenticationHolder;
+  }
 
-	/**
-	 * The authentication in place when the original access token was
-	 * created
-	 *
-	 * @return the authentication
-	 */
-	@ManyToOne
-	@JoinColumn(name = "auth_holder_id")
-	public AuthenticationHolderEntity getAuthenticationHolder() {
-		return authenticationHolder;
-	}
+  @Override
+  public String getValue() {
+    return value;
+  }
 
-	/**
-	 * @param authentication the authentication to set
-	 */
-	public void setAuthenticationHolder(AuthenticationHolderEntity authenticationHolder) {
-		this.authenticationHolder = authenticationHolder;
-	}
+  public void setValue(String value) {
+    this.value = value;
+  }
 
-	/**
-	 * Get the JWT-encoded value of this token
-	 */
-	@Override
-	@Transient
-	public String getValue() {
-		return jwt.serialize();
-	}
+  public Date getExpiration() {
+    return expiration;
+  }
 
-	@Basic
-	@Temporal(javax.persistence.TemporalType.TIMESTAMP)
-	@Column(name = "expiration")
-	public Date getExpiration() {
-		return expiration;
-	}
+  public void setExpiration(Date expiration) {
+    this.expiration = expiration;
+  }
 
-	/* (non-Javadoc)
-	 * @see org.springframework.security.oauth2.common.ExpiringOAuth2RefreshToken#setExpiration(java.util.Date)
-	 */
+  public ClientDetailsEntity getClient() {
+    return client;
+  }
 
-	public void setExpiration(Date expiration) {
-		this.expiration = expiration;
-	}
+  public void setClient(ClientDetailsEntity client) {
+    this.client = client;
+  }
 
-	/**
-	 * Has this token expired?
-	 * @return true if it has a timeout set and the timeout has passed
-	 */
-	@Transient
-	public boolean isExpired() {
-		return getExpiration() == null ? false : System.currentTimeMillis() > getExpiration().getTime();
-	}
-
-	/**
-	 * @return the client
-	 */
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "client_id")
-	public ClientDetailsEntity getClient() {
-		return client;
-	}
-
-	/**
-	 * @param client the client to set
-	 */
-	public void setClient(ClientDetailsEntity client) {
-		this.client = client;
-	}
-
-	/**
-	 * Get the JWT object directly
-	 * @return the jwt
-	 */
-	@Basic
-	@Column(name="token_value")
-	@Convert(converter = JWTStringConverter.class)
-	public JWT getJwt() {
-		return jwt;
-	}
-
-	/**
-	 * @param jwt the jwt to set
-	 */
-	public void setJwt(JWT jwt) {
-		this.jwt = jwt;
-	}
+  @Transient
+  public PlainJWT getJwt() {
+    try {
+      return PlainJWT.parse(value);
+    } catch (ParseException e) {
+      return null;
+    }
+  }
 
 }

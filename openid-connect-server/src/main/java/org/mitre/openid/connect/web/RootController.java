6faacc1a94e40360a79d4c2c3355ec0dc23cdd4e
enricovianello <maverick17586@gmail.com>
2026-07-03 10:35:41 +0200

    Remove ApprovedSite DeviceCode ans Stats stuff
    Keep the models
    Bump version to v2.2.0 20260703

--- openid-connect-server/src/main/java/org/mitre/openid/connect/web/RootController.java
+++ openid-connect-server/src/main/java/org/mitre/openid/connect/web/RootController.java
@@ -17,10 +17,6 @@
  *******************************************************************************/
 package org.mitre.openid.connect.web;
 
-import java.util.Map;
-
-import org.mitre.openid.connect.service.StatsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -33,46 +29,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RootController {
 
-	public static final String API_URL = "api";
+  public static final String API_URL = "api";
 
-	@Autowired
-	private StatsService statsService;
 
-	@RequestMapping({"", "home", "index"})
-	public String showHomePage(ModelMap m) {
-		return "home";
-	}
+  @RequestMapping({"", "home", "index"})
+  public String showHomePage(ModelMap m) {
+    return "home";
+  }
 
-	@RequestMapping({"about", "about/"})
-	public String showAboutPage(ModelMap m) {
-		return "about";
-	}
+  @RequestMapping({"about", "about/"})
+  public String showAboutPage(ModelMap m) {
+    return "about";
+  }
 
-	@RequestMapping({"stats", "stats/"})
-	public String showStatsPage(ModelMap m) {
-		Map<String, Integer> summary = statsService.getSummaryStats();
+  @RequestMapping({"contact", "contact/"})
+  public String showContactPage(ModelMap m) {
+    return "contact";
+  }
 
-		m.put("statsSummary", summary);
-		return "stats";
-	}
-
-	@RequestMapping({"contact", "contact/"})
-	public String showContactPage(ModelMap m) {
-		return "contact";
-	}
-
-	@PreAuthorize("hasRole('ROLE_USER')")
-	@RequestMapping("manage/**")
-	public String showClientManager(ModelMap m) {
-		return "manage";
-	}
-
-	public StatsService getStatsService() {
-		return statsService;
-	}
-
-	public void setStatsService(StatsService statsService) {
-		this.statsService = statsService;
-	}
+  @PreAuthorize("hasRole('ROLE_USER')")
+  @RequestMapping("manage/**")
+  public String showClientManager(ModelMap m) {
+    return "manage";
+  }
 
 }

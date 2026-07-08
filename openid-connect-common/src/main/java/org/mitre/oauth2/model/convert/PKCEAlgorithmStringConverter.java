743246eb0a6b2d714251a5798847c75cf2b310d6
Enrico Vianello <enrico.vianello@cnaf.infn.it>
2026-07-08 17:26:59 +0200

    Refactor PKCEAlgorithm and bump version to 2.1.1 20260706 (#39)

--- openid-connect-common/src/main/java/org/mitre/oauth2/model/convert/PKCEAlgorithmStringConverter.java
+++ openid-connect-common/src/main/java/org/mitre/oauth2/model/convert/PKCEAlgorithmStringConverter.java
@@ -21,32 +21,17 @@ import javax.persistence.Converter;
 
 import org.mitre.oauth2.model.PKCEAlgorithm;
 
-/**
- * @author jricher
- *
- */
 @Converter
 public class PKCEAlgorithmStringConverter implements AttributeConverter<PKCEAlgorithm, String> {
 
-	@Override
-	public String convertToDatabaseColumn(PKCEAlgorithm attribute) {
-		if (attribute != null) {
-			return attribute.getName();
-		} else {
-			return null;
-		}
-	}
+  @Override
+  public String convertToDatabaseColumn(PKCEAlgorithm attribute) {
+    return attribute != null ? attribute.name() : null;
+  }
 
-	/* (non-Javadoc)
-	 * @see javax.persistence.AttributeConverter#convertToEntityAttribute(java.lang.Object)
-	 */
-	@Override
-	public PKCEAlgorithm convertToEntityAttribute(String dbData) {
-		if (dbData != null) {
-			return PKCEAlgorithm.parse(dbData);
-		} else {
-			return null;
-		}
-	}
+  @Override
+  public PKCEAlgorithm convertToEntityAttribute(String dbData) {
+    return dbData != null ? PKCEAlgorithm.valueOf(dbData.toLowerCase()) : PKCEAlgorithm.optional;
+  }
 
 }
